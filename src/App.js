c8fac4649d74dc4e84a896c57dd72f19ec72f3ea
wt-854 <wt854@uowmail.edu.au>
2020-05-30 11:34:08 +0800

    done with hooks

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Pet from './Pet';
+import SearchParams from './SearchParams';
 
 const App = () => {
-  // return React.createElement("div", { id: "something-important" }, [
-  //   React.createElement("h1", {}, "Adopt Me!"),
-  //   React.createElement(Pet, {
-  //     id: 1,
-  //     name: "Luna",
-  //     animal: "Dog",
-  //     breed: "Havanese",
-  //   }),
-  //   React.createElement(Pet, {
-  //     id: 2,
-  //     name: "Pepper",
-  //     animal: "Bord",
-  //     breed: "Cockatiel",
-  //   }),
-  //   React.createElement(Pet, {
-  //     id: 3,
-  //     name: "Doink",
-  //     animal: "Cat",
-  //     breed: "Mixed",
-  //   }),
-  // ]);
   return (
     <div>
       <h1 id="something-important">Adopt me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese"/>
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel"/>
-      <Pet name="Doink" animal="Cat" breed="Mixed"/>
+      <SearchParams/>
     </div>
   );
 };
