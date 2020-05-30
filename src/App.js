7a56dc098a995eed87dfbd5b5a9845a6bce2cecd
wt-854 <wt854@uowmail.edu.au>
2020-05-30 14:15:34 +0800

    done w async - one-way data flow

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import { render } from 'react-dom';
-import SearchParams from './SearchParams';
+import React from "react";
+import { render } from "react-dom";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
-    <div>
-      <h1 id="something-important">Adopt me!</h1>
-      <SearchParams/>
-    </div>
+    <React.StrictMode>
+      <div>
+        <h1 id="something-important">Adopt me!</h1>
+        <SearchParams />
+      </div>
+    </React.StrictMode>
   );
 };
 
