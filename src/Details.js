561b34d73a1f178c9e77ab3b5b26977a30c91e0a
wt-854 <wt854@uowmail.edu.au>
2020-05-30 20:33:47 +0800

    context with classes

--- src/Details.js
+++ src/Details.js
@@ -1,13 +1,14 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
-import Carousel from './Carousel';
-import ErrorBoundary from './ErrorBoundary';
+import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
+import ThemeContext from "./ThemeContext";
 
 class Details extends React.Component {
-//   constructor() {
-//     super();
-//     this.state = { loading: true };
-//   }
+  //   constructor() {
+  //     super();
+  //     this.state = { loading: true };
+  //   }
 
   state = { loading: true };
 
@@ -42,7 +43,11 @@ class Details extends React.Component {
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} — ${breed} — ${location}`}</h2>
-          <button>Adopt {name}</button>
+          <ThemeContext.Consumer>
+            {([theme]) => (
+              <button style={{ backgroundColor: theme }}>Adopt {name}</button>
+            )}
+          </ThemeContext.Consumer>
           <p>{description}</p>
         </div>
       </div>
@@ -55,5 +60,5 @@ export default function DetailsWithErrorBoundary(props) {
     <ErrorBoundary>
       <Details {...props} />
     </ErrorBoundary>
-  )
-};
+  );
+}
